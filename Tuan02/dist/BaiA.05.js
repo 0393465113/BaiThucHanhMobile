e3c0e49def0dba1303ad9ad206aed94cc576c5bc
0393465113 <tranngochung19112004@gmail.com>
2025-08-30 13:10:50 +0700

    BaiA.05

---
+++ Tuan02/dist/BaiA.05.js
@@ -0,0 +1,9 @@
+"use strict";
+const simulateTask = (time) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Task done");
+        }, time);
+    });
+};
+simulateTask(2000).then((msg) => console.log(msg));
---
+++ Tuan02/src/BaiA.05.ts
@@ -0,0 +1,9 @@
+const simulateTask = (time: number): Promise<string> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Task done");
+    }, time);
+  });
+};
+
+simulateTask(2000).then((msg) => console.log(msg));
