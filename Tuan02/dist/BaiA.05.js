bdd9e87e5d951818490f50f3dc12ddef4beb7ec8
0393465113 <tranngochung19112004@gmail.com>
2025-08-30 13:37:32 +0700

    BaiA.07

--- Tuan02/dist/BaiA.05.js
+++ Tuan02/dist/BaiA.05.js
@@ -1,15 +1,26 @@
 "use strict";
-const simulateTask = (time) => {
+const simulateTask = (time, id) => {
     return new Promise((resolve) => {
         setTimeout(() => {
-            resolve("Task done");
+            resolve(`Task ${id} done`);
         }, time);
     });
 };
 Promise.all([
-    simulateTask(1000),
-    simulateTask(1000),
-    simulateTask(1000),
+    simulateTask(1000, 1),
+    simulateTask(1000, 2),
+    simulateTask(1000, 3),
 ]).then(results => {
     console.log("Kết quả:", results);
+});
+Promise.race([
+    simulateTask(2000, 1),
+    simulateTask(1000, 2),
+    simulateTask(3000, 3),
+])
+    .then(result => {
+    console.log("Kết quả đầu tiên:", result);
+})
+    .catch(err => {
+    console.error("Error:", err);
 });
--- Tuan02/src/BaiA.05.ts
+++ Tuan02/src/BaiA.05.ts
@@ -1,15 +1,26 @@
-const simulateTask = (time: number): Promise<string> => {
+const simulateTask = (time: number, id: number): Promise<string> => {
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve("Task done");
+      resolve(`Task ${id} done`);
     }, time);
   });
 };
 
 Promise.all([
-  simulateTask(1000),
-  simulateTask(1000),
-  simulateTask(1000),
+  simulateTask(1000, 1),
+  simulateTask(1000, 2),
+  simulateTask(1000, 3),
 ]).then(results => {
   console.log("Kết quả:", results);
 });
+Promise.race([
+  simulateTask(2000, 1),
+  simulateTask(1000, 2),
+  simulateTask(3000, 3),
+])
+  .then(result => {
+    console.log("Kết quả đầu tiên:", result);
+  })
+  .catch(err => {
+    console.error("Error:", err);
+  });
