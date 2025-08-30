12ce7c7e16b9ba472eb866b7e05e9755a67eacf2
0393465113 <tranngochung19112004@gmail.com>
2025-08-30 13:06:43 +0700

    BaiA.03

---
+++ Tuan02/src/BaiA.03.ts
@@ -0,0 +1,10 @@
+const rejectPromise = (): Promise<never> => {
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      reject(new Error("Something went wrong"));
+    }, 1000);
+  });
+};
+
+// Example usage
+rejectPromise().catch(err => console.error(err.message));
