5f1c58fa0bd88ea6454c7555697ee845d9992465
0393465113 <tranngochung19112004@gmail.com>
2025-08-30 13:02:02 +0700

    BaiA.02

---
+++ Tuan02/dist/BaiA.02.js
@@ -0,0 +1,9 @@
+"use strict";
+const getNumber = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(10);
+        }, 1000);
+    });
+};
+getNumber().then((num) => console.log(num));
---
+++ Tuan02/src/BaiA.02.ts
@@ -0,0 +1,9 @@
+const getNumber = (): Promise<number> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(10);
+    }, 1000); 
+  });
+};
+
+getNumber().then((num) => console.log(num)); 
